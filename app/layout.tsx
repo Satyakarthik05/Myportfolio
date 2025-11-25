00b4b1edb52df8592c1ff2c1e4f40b3e42816b87
satyakarthik <satyakarthikvelivela@gmail.com>
2025-11-25 22:59:29 +0530

    updated fev icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,17 +15,16 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
+        url: "/icon.png",
         media: "(prefers-color-scheme: light)",
       },
       {
-        url: "/icon-dark-32x32.png",
+        url: "/icon.png",
         media: "(prefers-color-scheme: dark)",
       },
       {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
+        url: "/icon.png", // <-- your PNG icon
+      }
     ],
     apple: "/apple-icon.png",
   },
